f020391c65dc52d0f9278f3c262cae355f306e43
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2023-11-16 20:11:13 -0300

    Configurando Headless

--- src/test/java/runner/support/BrowserFactory.java
+++ src/test/java/runner/support/BrowserFactory.java
@@ -8,20 +8,43 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserFactory
 {
-    public static WebDriver getBrowser()
+    public static enum Bronser {
+        CHROME,
+        CHROME_HEADLESS
+    }
+
+    public static WebDriver getBrowser() {
+        return getBrowser(Bronser.CHROME_HEADLESS);
+    }
+
+    public static WebDriver getBrowser(Bronser nav)
     {
-        //variaveis necessárias
         WebDriver navegador;
         ChromeOptions options;
 
-        // reduzindo output do webdriver
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+        switch (nav) {
+            case CHROME:
+                System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+                WebDriverManager.chromedriver().setup();
+                options = getChromeOptions();
+                navegador = new ChromeDriver(options);
+                navegador.manage().window().maximize();
+                break;
+            case CHROME_HEADLESS:
+                System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+                WebDriverManager.chromedriver().setup();
+                options = getChromeHeadlessOptions();
+                navegador = new ChromeDriver(options);
+                break;
+            default:
+                navegador = null;
+                new Exception("Navegador nao suportado");
+        }
+        return navegador;
+    }
 
-        //Webdriver do chrome
-        WebDriverManager.chromedriver().setup();
-        options = new ChromeOptions();
-
-        //Inicio
+    private static ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-crash-reporter");
         options.addArguments("--disable-extensions");
@@ -31,9 +54,23 @@ public class BrowserFactory
         options.addArguments("--disable-notifications");
         options.addArguments("--log-level=3");
         options.addArguments("--output=/dev/null");
+        return options;
+    }
 
-        navegador = new ChromeDriver(options);
-        navegador.manage().window().maximize();
-        return navegador;
+    private static ChromeOptions getChromeHeadlessOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-crash-reporter");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-in-process-stack-traces");
+        options.addArguments("--disable-logging");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--log-level=3");
+        options.addArguments("--output=/dev/null");
+        return options;
     }
 }
