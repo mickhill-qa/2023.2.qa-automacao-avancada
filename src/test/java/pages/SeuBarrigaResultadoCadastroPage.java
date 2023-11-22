5f843c00e97fb9f5008be44f7651b3c0de79bafd
Ingrid Rana <ingridranast@gmail.com>
2023-11-21 23:41:23 -0300

    Automatizacao de cadastro

---
+++ src/test/java/pages/SeuBarrigaResultadoCadastroPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.By;
+import runner.base_class.BasePage;
+
+public class SeuBarrigaResultadoCadastroPage extends BasePage {
+    private String url = "https:/seubarriga.wcaquino.me/cadastrarUsuario";
+    private By mensagemSucesso = By.cssSelector("div.alert.alert-success");
+    private By mensagemError = By.cssSelector("div.alert.alert-danger");
+
+    public Boolean verificarSeEstouNaPagina(){
+        String urlAtual = driver.getCurrentUrl();
+        return urlAtual.contains(url);
+    }
+    public String getMensagemSucesso(){
+        String text = driver.findElement(mensagemSucesso).getText();
+        return text;
+    }
+    public String getMensagemError(){
+        String text = driver.findElement(mensagemError).getText();
+        return text;
+    }
+}
