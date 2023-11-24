32396e4a1e65fa6c20e89d85d2c014eed0c47664
Renato Correa <renatojpcorrea@gmail.com>
2023-11-23 22:07:20 -0300

    Adicionado em todos os steps , renato para nÃao dar conflito

--- src/test/java/steps/CadastroSeuBarrigaSteps.java
+++ src/test/java/steps/CadastroSeuBarrigaSteps.java
@@ -11,12 +11,12 @@ public class CadastroSeuBarrigaSteps extends BaseSteps {
 
     CadastroPage cadastroPage = new CadastroPage();
 
-    @Dado("que o usuario acesse o site")
+    @Dado("que o usuario acesse o site, renato")
     public void que_o_usuario_acesse_o_site() {
         cadastroPage.validaPaginaCadastro();
     }
 
-    @Quando("preencher os campos nome , email , senha e clicar no botao Cadastrar")
+    @Quando("preencher os campos nome , email , senha e clicar no botao Cadastrar, renato")
     public void preencher_os_campos_nome_email_senha_e_clicar_no_botao_cadastrar() {
         Random rand = new Random();
         String nome = "Renato";
@@ -26,12 +26,12 @@ public class CadastroSeuBarrigaSteps extends BaseSteps {
         cadastroPage.preencherCadastro(nome, email, senha);
 
     }
-    @Entao("o sistema mostrara a mensagem de erro")
+    @Entao("o sistema mostrara a mensagem de erro, renato")
     public void o_sistema_mostrara_a_mensagem_de_erro() {
         cadastroPage.validarCadastro();
     }
 
-    @Quando("preencher os campos nome e email, deixar o campo senha em branco e clicar no botao Cadastrar")
+    @Quando("preencher os campos nome e email, deixar o campo senha em branco e clicar no botao Cadastrar, renato")
     public void preencher_os_campos_nome_e_email_deixar_o_campo_senha_em_branco_e_clicar_no_botao_cadastrar() {
         cadastroPage.preencherCadastroSemSenha("Renato", "testando@testesemsenha.com");
     }
--- src/test/resources/features/cadastroSeuBarriga.feature
+++ src/test/resources/features/cadastroSeuBarriga.feature
@@ -4,15 +4,15 @@ Funcionalidade: Cadastro no site "https://seubarriga.wcaquino.me/" para ter aces
 
   Cenario: Tentar realizar cadastro com senha contendo apenas numeros
 
-    Dado que o usuario acesse o site
-    Quando preencher os campos nome , email , senha e clicar no botao Cadastrar
-    Entao o sistema mostrara a mensagem de erro
+    Dado que o usuario acesse o site, renato
+    Quando preencher os campos nome , email , senha e clicar no botao Cadastrar, renato
+    Entao o sistema mostrara a mensagem de erro, renato
 
   Cenario: Tentar realizar cadastro com senha em branco
 
-    Dado que o usuario acesse o site
-    Quando preencher os campos nome e email, deixar o campo senha em branco e clicar no botao Cadastrar
-    Entao o sistema mostrara a mensagem de erro
+    Dado que o usuario acesse o site, renato
+    Quando preencher os campos nome e email, deixar o campo senha em branco e clicar no botao Cadastrar, renato
+    Entao o sistema mostrara a mensagem de erro, renato
 
 
 
