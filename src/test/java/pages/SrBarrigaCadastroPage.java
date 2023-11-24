1c34090fa18449ff9549c2249307b7b3d7c68dcc
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2023-11-23 21:03:56 -0300

    Step 4: Mapeando o objeto de teste

--- src/test/java/pages/SrBarrigaCadastroPage.java
+++ src/test/java/pages/SrBarrigaCadastroPage.java
@@ -1,6 +1,14 @@
 package pages;
 
+import org.openqa.selenium.By;
 import runner.base_class.BasePage;
 
 public class SrBarrigaCadastroPage extends BasePage {
+    private String url = "https://seubarriga.wcaquino.me/cadastro";
+    private By inputNome = By.id("nome");
+    private By inputEmail = By.id("email");
+    private By inputSenha = By.id("senha");
+    private By btnCadastrar = By.cssSelector("input[value='Cadastrar']");
+    private By textMensagem = By.cssSelector("div[role='alert']");
+
 }
