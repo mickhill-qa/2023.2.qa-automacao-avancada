8532627132afbf52e88eee78660eefe8e36181af
José Winny <jose.winny.dev@gmail.com>
2023-11-24 19:26:37 -0300

    Update CadastroSeuBarrigaPage.java

--- src/test/java/josewinny/pages/CadastroSeuBarrigaPage.java
+++ src/test/java/josewinny/pages/CadastroSeuBarrigaPage.java
@@ -1,7 +1,10 @@
 package josewinny.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import runner.base_class.BasePage;
+
+import java.util.List;
 
 
 public class CadastroSeuBarrigaPage extends BasePage {
@@ -12,6 +15,7 @@ public class CadastroSeuBarrigaPage extends BasePage {
     private By inputSenha = By.id("senha");
     private By btnCadastrar = By.className("btn");
     private By textNotificacao = By.cssSelector("div[role='alert']");
+    private List<String> todasMensagemDeNotificacao;
 
     public void abrir() {
         driver.get(url);
@@ -36,6 +40,18 @@ public class CadastroSeuBarrigaPage extends BasePage {
     public Boolean verificarSeEstouNapagina() {
         String urlAtual = driver.getCurrentUrl();
         return urlAtual.contains(url);
+    }
+
+    public void pegarTodasNotificacoes() {
+        waitElementVisible(textNotificacao, 5);
+        List<WebElement> listaDeElementos = driver.findElements(textNotificacao);
+        for (WebElement element : listaDeElementos) {
+            todasMensagemDeNotificacao.add(element.getText());
+        }
+    }
+
+    public Boolean contemMensagem(String _mensagem) {
+        return todasMensagemDeNotificacao.contains(_mensagem);
     }
 
     public Boolean validaNotificacao(String _mensagem) {
