963ac285eed9f5912dcf14dd2788120733415081
AndreL0M <marquesandre.qa@gmail.com>
2023-11-24 23:21:29 -0300

    Automacao dos 2 cenarios Seu Barriga

--- src/test/java/steps/CadastroSeuBarrigaSteps.java
+++ src/test/java/steps/CadastroSeuBarrigaSteps.java
@@ -1,24 +1,55 @@
 package steps;
 
-import io.cucumber.java.pt.*;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
 import org.junit.Assert;
-import org.openqa.selenium.WebElement;
-import pages.GooglePesquisaPage;
-import pages.GoogleResultadosPesquisaPage;
-import runner.base_class.BaseSteps;
-import java.util.List;
+import pages.CadastroSeuBarrigaPage;
+import java.util.UUID;
 
-public class CadastroSeuBarrigaSteps extends BaseSteps{
+public class CadastroSeuBarrigaSteps {
+
+    CadastroSeuBarrigaPage paginaCadastro = new CadastroSeuBarrigaPage();
+
+        @Dado("que o usuario esta na pagina de cadastro")
+        public void queOUsuarioEstaNaPaginaDeCadastro() {
+            paginaCadastro.abrir();
+        }
+
+    @E("o usuario preeche os campos Email e Senha")
+    public void oUsuarioPreecheOsCamposEmailESenha() {
+        paginaCadastro.preencherNome("Testador da Silva");
+        paginaCadastro.preencherEmail("testador" + UUID.randomUUID().toString() + "@email.com");
+        paginaCadastro.preencherSenha("123456");
+    }
+
+
+    @Quando("o usuario clica no botao CADASTRAR")
+    public void oUsuarioClicaNoBotaoCADASTRAR() {
+        paginaCadastro.clicarCadastrar();
+    }
 
 
 
 
+    @Entao("o sistema mostra uma mensagem de sucesso")
+    public void oSistemaMostraUmaMensagemDeSucesso() {
+        String msgSucesso = paginaCadastro.buscarMsg();
+        String mensagemEsperada = "Usuário inserido com sucesso";
+        Assert.assertEquals(mensagemEsperada, msgSucesso);
+    }
 
 
+    @Entao("o sistema mostra uma mensagem de erro informando os campos obrigatorios")
+    public void oSistemaMostraUmaMensagemDeErroInformandoOsCamposObrigatorios() {
+        String mensagemErro = paginaCadastro.buscarMsgErro();
 
+        Assert.assertTrue(mensagemErro.contains("Nome é um campo obrigatório"));
 
+        Assert.assertTrue(mensagemErro.contains("Email é um campo obrigatório"));
+        
+        Assert.assertTrue(mensagemErro.contains("Senha é um campo obrigatório"));
 
-
-
-
+    }
 }
--- src/test/resources/features/CadastroSeuBarriga.feature
+++ src/test/resources/features/CadastroSeuBarriga.feature
@@ -5,8 +5,8 @@ Funcionalidade: Cadastro Seu Barriga
       Dado que o usuario esta na pagina de cadastro
       E o usuario preeche os campos Email e Senha
       Quando o usuario clica no botao CADASTRAR
-      Entao o sistema redireciona para a pagina de Login
-      E o sistema mostra uma mensagem de sucesso
+      Entao o sistema mostra uma mensagem de sucesso
+
 
     Cenario: Cadastro sem preencher os campos obrigatorios
       Dado que o usuario esta na pagina de cadastro
