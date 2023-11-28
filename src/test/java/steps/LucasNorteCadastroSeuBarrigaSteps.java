9ff3fa80d9e3313b9e0bb288016601b293c0150f
Lucas Norte - Ubuntu <lucas10norte@gmail.com>
2023-11-27 21:18:16 -0300

    :test_tube: Step 2: Gerar scripts para programar os testes

---
+++ src/test/java/steps/LucasNorteCadastroSeuBarrigaSteps.java
@@ -0,0 +1,28 @@
+package steps;
+
+import io.cucumber.java.pt.*;
+
+public class LucasNorteCadastroSeuBarrigaSteps {
+    
+    @Dado("que o usuario esta na tela de cadastro, ln")
+    public void que_o_usuario_esta_na_tela_de_cadastro_ln() {
+    }
+    @Quando("o usuario preenche os campos obrigatorios com dados validos, ln")
+    public void o_usuario_preenche_os_campos_obrigatorios_com_dados_validos_ln() {
+    }
+    @E("o usuario clica no botao Cadastrar, ln")
+    public void o_usuario_clica_no_botao_cadastrar_ln() {
+    }
+    @Entao("o sistema exibi a mensagem {string}, ln")
+    public void o_sistema_exibi_a_mensagem_ln(String string) {
+    }
+
+    @Quando("o usuario deixa o campo nome vazio, ln")
+    public void o_usuario_deixa_o_campo_nome_vazio_ln() {
+    }
+
+    @Quando("o usuario preenche os campos email e senha com dados validos, ln")
+    public void o_usuario_preenche_os_campos_email_e_senha_com_dados_validos_ln() {
+    }
+
+}
