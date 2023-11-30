2f03686976acadfa5ee43694bbc35477c762c47c
felipedanielsousa <felipedanielsousa@gmail.com>
2023-11-30 02:40:08 -0300

    mapeando elementos cenario 3, step 4

--- src/test/java/pages/AutomationExampleInvoiceListPage.java
+++ src/test/java/pages/AutomationExampleInvoiceListPage.java
@@ -4,15 +4,36 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import runner.base_class.BasePage;
 
+import java.util.ArrayList;
+
 public class AutomationExampleInvoiceListPage extends BasePage {
     //mapping
     private String urlInvoiceList = "https://automation-sandbox-python-mpywqjbdza-uc.a.run.app/account";
     private By invoiceListTitle = By.xpath("//h2[contains(text(), \"Invoice List\")]");
+    private By firstInvoiceLink = By.cssSelector("a[href='/invoice/0']");
+    //Elements TesCase 03
+    private By hotelName = By.xpath("//h4[contains(text(), 'Rendezvous Hotel')]");
+    private By invoiceDate = By.xpath("//li[contains(text(), ' 14/01/2018')]");
+    private By dueDate = By.xpath("//li[contains(text(), ' 15/01/2018')]");
+    private By invoiceNumber = By.xpath("//h6[contains(text(),'Invoice #110')]");
+    private By BookingCode = By.xpath("");
+
     //acctions
     public String getTittleInvoiceList() {
         waitElementVisible(invoiceListTitle, 5);
         return driver.findElement(invoiceListTitle).getText();
     }
+    public void clickFirstInvoiceLink(){
+        waitElementVisible(firstInvoiceLink, 4);
+        driver.findElement(firstInvoiceLink).click();
+    }
+    public void switchWindow(){
+        ArrayList<String> abas = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(abas.get(1));
 
+    }
+    public void getElementsValidationInvoiceDetails(){
+
+    }
 
 }
--- src/test/java/steps/InvoiceDetailsSteps.java
+++ src/test/java/steps/InvoiceDetailsSteps.java
@@ -1,14 +1,24 @@
 package steps;
 import io.cucumber.java.en.*;
+import pages.AutomationExampleInvoiceListPage;
+import pages.AutomationExampleLoginPage;
+
 public class InvoiceDetailsSteps {
+    AutomationExampleLoginPage loginPage = new AutomationExampleLoginPage();
+    AutomationExampleInvoiceListPage invoiceListPage = new AutomationExampleInvoiceListPage();
     @Given("that the user is logged into the system and the user is on the Invoice List screen")
     public void that_the_user_is_logged_into_the_system_and_the_user_is_on_the_invoice_list_screen() {
+        loginPage.abrir();
+        loginPage.fillUsernameAndPasswordFields("demouser", "abc123");
+        loginPage.clickButton();
     }
     @When("the user clicks on the first invoice details link displayed on the screen")
     public void the_user_clicks_on_the_first_invoice_details_link_displayed_on_the_screen() {
+        invoiceListPage.clickFirstInvoiceLink();
     }
     @Then("the user should be redirected to the invoice details screen")
     public void the_user_should_be_redirected_to_the_invoice_details_screen() {
+        invoiceListPage.switchWindow();
     }
     @Then("the system should match the data provided in the table.")
     public void the_system_should_match_the_data_provided_in_the_table() {
