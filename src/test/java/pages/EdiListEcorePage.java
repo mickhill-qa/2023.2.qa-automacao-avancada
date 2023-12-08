fc3aed2e9528b57ee263818214cabd5b444bd757
Edinete Sousa <edineteb.sousa@gmail.com>
2023-12-08 14:16:36 -0300

    Page de List

--- src/test/java/pages/EdiListEcorePage.java
+++ src/test/java/pages/EdiListEcorePage.java
@@ -3,14 +3,11 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import runner.base_class.BasePage;
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class EdiListEcorePage extends BasePage {
     private String url = "https://automation-sandbox-python-mpywqjbdza-uc.a.run.app/account";
+    private By clickInvoiceDetails = By.cssSelector("div.row > div.col.border > a");
 
     public boolean invoiceList() {
         String urlAtual = driver.getCurrentUrl();
@@ -18,18 +15,9 @@ public class EdiListEcorePage extends BasePage {
             return true;
         else
             return false;
-
     }
-
-    public void clickInvoice() {
-        WebElement invoiceLink = driver.findElement(By.xpath("(//a)[2]"));
-        invoiceLink.click();
-
+    public void clickInvoiceDetails(int index) {
+        List<WebElement> element = driver.findElements(clickInvoiceDetails);
+        element.get(index - 1).click();
     }
-
-    public void validarNovaAba() {
-        List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(windowHandles.get(1));
-    }
-
 }
