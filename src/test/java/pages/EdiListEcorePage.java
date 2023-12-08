003f97c5669793864994a5da0f0f174e50393b68
Edinete Sousa <edineteb.sousa@gmail.com>
2023-12-08 16:43:37 -0300

    Alterando Page de list

--- src/test/java/pages/EdiListEcorePage.java
+++ src/test/java/pages/EdiListEcorePage.java
@@ -3,10 +3,15 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import runner.base_class.BasePage;
+import runner.support.Utils;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class EdiListEcorePage extends BasePage {
+public class EdiListEcorePage extends Utils {
     private String url = "https://automation-sandbox-python-mpywqjbdza-uc.a.run.app/account";
+
+    private By linkDetails = By.xpath("(//a)[2]");
     private By clickInvoiceDetails = By.cssSelector("div.row > div.col.border > a");
 
     public boolean invoiceList() {
@@ -16,8 +21,8 @@ public class EdiListEcorePage extends BasePage {
         else
             return false;
     }
-    public void clickInvoiceDetails(int index) {
-        List<WebElement> element = driver.findElements(clickInvoiceDetails);
-        element.get(index - 1).click();
+    public void clikLinkInvoiceDetails(){
+        click_LinkDetails(clickInvoiceDetails);
+        getNewTab();
     }
 }
