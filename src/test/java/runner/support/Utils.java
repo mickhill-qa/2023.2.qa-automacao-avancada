e475c0a28ae054910c42fbcae679a4aec10322fb
Edinete Sousa <edineteb.sousa@gmail.com>
2023-12-08 16:41:39 -0300

    Alterando Utils

--- src/test/java/runner/support/Utils.java
+++ src/test/java/runner/support/Utils.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import runner.base_class.BasePage;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,18 +14,33 @@ public class Utils extends BasePage {
         String primeiroNome = nomes.get(posicao);
         return primeiroNome;
     }
+    public void getNewTab(){
+        List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windowHandles.get(1));
 
-    public boolean obterValor(String texto) {
-        return driver.getPageSource().contains(texto);
     }
+    public void click_LinkDetails(By element) {
+        WebElement invoiceLink = driver.findElement(element);
+        invoiceLink.click();
+    }
+//    public boolean obterValor(String texto) {
+//        return driver.getPageSource().contains(texto);
+//    }
+
     public String get_text(By element) {
-       return driver.findElement(element).getText();
+        return driver.findElement(element).getText();
     }
-    public void click(By element){
+
+    public void click(By element) {
         driver.findElement(element).click();
     }
-    public String get_text_index(By element, int index){
+
+    public String get_text_index(By element, int index) {
         return driver.findElements(element).get(index).getText();
+    }
+    public void clicknolink(By element) {
+        WebElement invoiceLink = driver.findElement(element);
+        invoiceLink.click();
     }
 }
 
