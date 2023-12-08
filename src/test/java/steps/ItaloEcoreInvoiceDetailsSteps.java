26c4e393411c462bbf1c9810de2f42a55ec6b271
ÍtaloFreitas95 <italorodriguesdefreitas95@gmail.com>
2023-12-08 20:23:41 -0300

    Atividade 2 entregue

--- src/test/java/steps/ItaloEcoreInvoiceDetailsSteps.java
+++ src/test/java/steps/ItaloEcoreInvoiceDetailsSteps.java
@@ -1,99 +1,130 @@
 package steps;
 
+import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 import org.junit.Assert;
 import pages.ItaloEcoreInvoiceDetailsPage;
-import pages.ItaloEcoreInvoiceListPage;
+import pages.ItaloEcoreLoginPage;
 import runner.base_class.BaseSteps;
 
 
 public class ItaloEcoreInvoiceDetailsSteps extends BaseSteps {
-    ItaloEcoreInvoiceListPage paginaListaFatura = new ItaloEcoreInvoiceListPage();
-    ItaloEcoreInvoiceDetailsPage paginaDetalheFatura = new ItaloEcoreInvoiceDetailsPage();
+    ItaloEcoreInvoiceDetailsPage invoicePage = new ItaloEcoreInvoiceDetailsPage();
+    ItaloEcoreLoginPage loginPage = new ItaloEcoreLoginPage();
 
-    @Quando("o usuario clicar no Invoice Link")
-    public void o_usuario_clicar_no_invoice_linki()
-    {
-        paginaListaFatura.clickInvoiceDetails(1);
+    @Dado("que o usuario logado esta na pagina de listagem de Invoice")
+    public void que_o_usuario_logado_esta_na_pagina_de_listagem_de_Invoice(){
+        loginPage.abrir();
+        loginPage.preencherUsername("demouser");
+        loginPage.preencherPassword("abc123");
+        loginPage.clicarBtnLogin();
+
+        Assert.assertTrue(invoicePage.isInvoiceListUrl());
     }
 
-    @Quando("o sistama mostra a pagina Invoice Details")
-    public void o_sistama_mostra_a_pagina_invoice_detailsi()
-    {
-        paginaDetalheFatura.estouAqui();
-        screenshot();
+    @Quando("o usuario clicar no link do hotel")
+    public void o_usuario_clicar_no_link_do_hotel(){
+        invoicePage.clickInvoiceDetailsLink();
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Hotel Name com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_hotel_name_com(String _hotelName)
-    {
-        String textoNatela = paginaDetalheFatura.getTextHotelName();
-        Assert.assertEquals(_hotelName, textoNatela);
+    @Entao("o sistema redireciona o usuario para a pagina de detalhes")
+    public void o_sistema_redireciona_o_usuario_para_a_pagina_de_detalhes(){
+        invoicePage.switchToInvoiceDetailsPage();
+        invoicePage.isInvoiceDetailsUrl();
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Invoice Date com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_invoice_date_com(String string)
-    {
+    @Entao("mostra {string} no campo Hotel name")
+    public void mostra_string_no_campo_Hotel_nome(String _hotelName){
+        String hotelName = invoicePage.getHotelName();
+        Assert.assertEquals(hotelName, _hotelName);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Due Date com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_due_date_com(String string)
-    {
+    @Entao("mostra {string} no campo Invoice date")
+    public void mostra_string_no_campo_Invoice_date(String _invoiceDate){
+        String invoiceDate = invoicePage.getInvoiceDate();
+        Assert.assertEquals(invoiceDate, _invoiceDate);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Invoice Number com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_invoice_number_com(String string)
-    {
+    @Entao("mostra {string} no campo Due Date")
+    public void mostra_string_no_campo_Due_Date(String _dueDate){
+        String dueDate = invoicePage.getDueDate();
+        Assert.assertEquals(dueDate, _dueDate);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Booking Code com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_booking_code_com(String string)
-    {
+    @Entao("mostra {string} no campo Invoice Number")
+    public void mostra_string_no_campo_Invoice_Number(String _invoiceNumber){
+        String invoiceNumber = invoicePage.getInvoiceNumber();
+        Assert.assertEquals(invoiceNumber, _invoiceNumber);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Customer Details com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_customer_details_com(String string)
-    {
+    @Entao("mostra {string} no campo Booking Code")
+    public void mostra_string_no_campo_booking_code(String _bookingCode){
+        String bookingCode = invoicePage.getBookingDetails().get("Booking Code");
+        Assert.assertNotNull(bookingCode);
+        Assert.assertEquals(_bookingCode, bookingCode);
+
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Room com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_room_com(String string)
-    {
+    @Entao("mostra {string} no campo Customer Details")
+    public void mostra_string_no_campo_customer_details(String _customerDestails){
+        String customerDestails = invoicePage.getCustomerDetails();
+        Assert.assertEquals(customerDestails,_customerDestails);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Check In com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_check_in_com(String string)
-    {
+    @Entao("mostra {string} no campo Room")
+    public void mostra_string_no_campo_room(String _room){
+        String roomInfo = invoicePage.getBookingDetails().get("Room");
+        Assert.assertNotNull(roomInfo);
+        Assert.assertEquals(_room, roomInfo);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Check Out com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_check_out_com(String string)
-    {
+    @Entao("mostra {string} no campo Total Stay Count")
+    public void mostra_string_no_campo_total_stay_count(String _totalStayCount){
+        String stayCountInfo = invoicePage.getBookingDetails().get("Total Stay Count");
+        Assert.assertNotNull(stayCountInfo);
+        Assert.assertEquals(_totalStayCount, stayCountInfo);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Total Stay Count com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_total_stay_count_com(String string)
-    {
+    @Entao("mostra {string} no campo Total Stay Amount")
+    public void mostra_string_no_campo_total_stay_amount(String _totalStayAmount){
+        String stayAmount = invoicePage.getBookingDetails().get("Total Stay Amount");
+        Assert.assertNotNull(stayAmount);
+        Assert.assertEquals(_totalStayAmount, stayAmount);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Total Stay Amount com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_total_stay_amount_com(String string)
-    {
+    @Entao("mostra {string} no campo Check In")
+    public void mostra_string_no_campo_check_in(String _checkIn){
+        String checkIn = invoicePage.getBookingDetails().get("Check-In");
+        Assert.assertNotNull(checkIn);
+        Assert.assertEquals(_checkIn, checkIn);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Deposit Now com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_deposit_now_com(String string)
-    {
+    @Entao("mostra {string} no campo Check Out")
+    public void mostra_string_no_campo_check_out(String _checkOut){
+        String ckeckOut = invoicePage.getBookingDetails().get("Check-Out");
+        Assert.assertNotNull(ckeckOut);
+        Assert.assertEquals(_checkOut, ckeckOut);
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Tax & VATe com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_tax_va_te_com(String string)
-    {
+    @Entao("mostra {string} no campo Deposit Nowt")
+    public void mostra_string_no_campo_deposit_nowt(String _depositNowt){
+        String depositNowt = invoicePage.getBillingDetails().get("Deposit Nowt");
+        Assert.assertNotNull(depositNowt);
+        Assert.assertEquals(_depositNowt, depositNowt);
+    }
+    @Entao("mostra {string} no campo Tax & VAT")
+    public void mostra_string_no_campo_tax_and_vat(String _taxAndVat){
+        String taxAndVat = invoicePage.getBillingDetails().get("Tax&VAT");
+        Assert.assertNotNull(taxAndVat);
+        Assert.assertTrue(_taxAndVat.contains(taxAndVat));
     }
 
-    @Entao("a pagina Invoice Details mostra o campo Total Amount com {string}")
-    public void a_pagina_invoice_details_mostra_o_campo_total_amount_com(String string)
-    {
+    @Entao("mostra {string} no campo Total Amount")
+    public void mostra_string_no_campo_total_Amount(String _totalAmount){
+        String totalAmount = invoicePage.getBillingDetails().get("Total Amount");
+        Assert.assertNotNull(totalAmount);
+        Assert.assertTrue(_totalAmount.contains(totalAmount));
     }
 }
+
--- src/test/resources/features/Italo_ecore_invoice_details.feature
+++ src/test/resources/features/Italo_ecore_invoice_details.feature
@@ -1,24 +1,21 @@
 #language: pt
 @resetBrowser
-Funcionalidade: Italo Invoice Details
-
-  Cenario: Validar Invoice Details, ln
-    Dado que o usuario esta na pagina de login
-    E o usuario preencher as credenciais "demouser" e "abc123"
-    E o sistema encaminha para a lista de faturas
-    Quando o usuario clicar no Invoice Link
-    E o sistama mostra a pagina Invoice Details
-    Entao a pagina Invoice Details mostra o campo Hotel Name com "Rendezvous Hotel"
-    E a pagina Invoice Details mostra o campo Invoice Date com ""
-    E a pagina Invoice Details mostra o campo Hotel Name com {string} o campo Due Date com ""
-    E a pagina Invoice Details mostra o campo Invoice Number com ""
-    E a pagina Invoice Details mostra o campo Booking Code com ""
-    E a pagina Invoice Details mostra o campo Customer Details com ""
-    E a pagina Invoice Details mostra o campo Room com ""
-    E a pagina Invoice Details mostra o campo Check In com ""
-    E a pagina Invoice Details mostra o campo Check Out com ""
-    E a pagina Invoice Details mostra o campo Total Stay Count com ""
-    E a pagina Invoice Details mostra o campo Total Stay Amount com ""
-    E a pagina Invoice Details mostra o campo Deposit Now com ""
-    E a pagina Invoice Details mostra o campo Tax & VATe com ""
-    E a pagina Invoice Details mostra o campo Total Amount com ""
+Funcionalidade: Validar informações de Invoice
+  Cenário: Detalhes do Invoice
+    Dado que o usuario logado esta na pagina de listagem de Invoice
+    Quando o usuario clicar no link do hotel
+    Entao o sistema redireciona o usuario para a pagina de detalhes
+    E mostra "Rendezvous Hotel" no campo Hotel name
+    E mostra "14/01/2018" no campo Invoice date
+    E mostra "15/01/2018" no campo Due Date
+    E mostra "110" no campo Invoice Number
+    E mostra "0875" no campo Booking Code
+    E mostra "JOHNY SMITH R2, AVENUE DU MAROC 123456" no campo Customer Details
+    E mostra "Superior Double" no campo Room
+    E mostra "1" no campo Total Stay Count
+    E mostra "$150" no campo Total Stay Amount
+    E mostra "14/01/2018" no campo Check In
+    E mostra "15/01/2018" no campo Check Out
+    E mostra "USD $20.90" no campo Deposit Nowt
+    E mostra "USD $19.00" no campo Tax & VAT
+    E mostra "USD $209.00" no campo Total Amount
