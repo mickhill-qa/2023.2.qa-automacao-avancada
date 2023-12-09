1dc14d78ebd4d5c3cd7a481447ece0a7fa78bb85
AlexsandroAmancio85 <alexsandroamancio85@gmail.com>
2023-12-08 22:51:56 -0300

    execucao dos steps e pages

--- src/test/java/pages/AlexsandroInvoiceDetailsEcore2Page.java
+++ src/test/java/pages/AlexsandroInvoiceDetailsEcore2Page.java
@@ -8,6 +8,19 @@ import java.util.ArrayList;
 public class AlexsandroInvoiceDetailsEcore2Page extends BasePage {
 
     private By textHotelName = By.cssSelector("h4.mt-5");
+    private By textInvoiceDate = By.cssSelector("li > span.font-weight-bold > 14/01/2018");
+    private By textDueDate = By.cssSelector("li > span.font-weight-bold > 15/01/2018");
+    private By textInvoiceNumber = By.cssSelector("h6.mt-2");
+    private By textBookingCode = By.cssSelector("h5.mt-5 > td > 0875");
+    private By textCustomerDetails = By.cssSelector("h5.mt-5 > div");
+    private By textRoom = By.cssSelector("h5.mt-5 > td > Superior Double");
+    private By textCheckIn = By.cssSelector("h5.mt-5 > td > 14/01/2018");
+    private By textCheckOut = By.cssSelector("h5.mt-5 > td > 15/01/2018");
+    private By totalStayCount = By.cssSelector("h5.mt-5 > td > 1");
+    private By totalStayAmount = By.cssSelector("h5.mt-5 > td > $150");
+    private By depositNowt = By.cssSelector("h5.mt-5 > td > USD $20.90");
+    private By taxVat = By.cssSelector("h5.mt-5 > td > USD $19");
+    private By totalAmount = By.cssSelector("h5.mt-5 > td > USD $209");
 
     public void iamHere()
     {
@@ -19,4 +32,17 @@ public class AlexsandroInvoiceDetailsEcore2Page extends BasePage {
     {
         return driver.findElement(textHotelName).getText();
     }
+    public String getTextInvoiceDate() { return driver.findElement(textInvoiceDate).getText(); }
+    public String getTextDueDate() { return driver.findElement(textDueDate).getText(); }
+    public String getTextInvoiceNumber() { return driver.findElement(textInvoiceNumber).getText(); }
+    public String getTextBookingCode() { return driver.findElement(textBookingCode).getText(); }
+    public String getTextCustomerDetails()  { return driver.findElement(textCustomerDetails).getText(); }
+    public String getTextRoom() { return driver.findElement(textRoom).getText(); }
+    public String getCheckIn() { return driver.findElement(textCheckIn).getText(); }
+    public String getCheckOut() { return driver.findElement(textCheckOut).getText(); }
+    public String getStayCount() { return driver.findElement(totalStayCount).getText(); }
+    public String getStayAmount() { return driver.findElement(totalStayAmount).getText(); }
+    public String getBillingDetails() { return driver.findElement(depositNowt).getText(); }
+    public String getTaxVat() { return driver.findElement(taxVat).getText(); }
+    public String getTotalAmount() { return driver.findElement(totalAmount).getText(); }
 }
--- src/test/java/steps/AlexsandroEcore2InvoiceDetailsSteps.java
+++ src/test/java/steps/AlexsandroEcore2InvoiceDetailsSteps.java
@@ -21,7 +21,6 @@ public class AlexsandroEcore2InvoiceDetailsSteps extends BaseSteps {
     public void o_sistama_exibe_a_pagina_invoice_details()
     {
         pageDetailFatura.iamHere();
-
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Hotel Name com {string}")
@@ -32,68 +31,94 @@ public class AlexsandroEcore2InvoiceDetailsSteps extends BaseSteps {
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Invoice Date com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_invoice_date_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_invoice_date_com(String _invoiceDate)
     {
+        String textInvoiceDate = pageDetailFatura.getTextInvoiceDate();
+        Assert.assertEquals(_invoiceDate, textInvoiceDate);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Due Date com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_due_date_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_due_date_com(String _dueDate)
     {
+        String textDueDate = pageDetailFatura.getTextDueDate();
+        Assert.assertEquals(_dueDate, textDueDate);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Invoice Number com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_invoice_number_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_invoice_number_com(String _invoiceNumber)
     {
+        String textInvoiceNumber = pageDetailFatura.getTextInvoiceNumber();
+        Assert.assertEquals(_invoiceNumber, textInvoiceNumber);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Booking Code com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_booking_code_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_booking_code_com(String _bookingCode)
     {
+        String textBookingCode = pageDetailFatura.getTextBookingCode();
+        Assert.assertEquals(_bookingCode, textBookingCode);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Customer Details com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_customer_details_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_customer_details_com(String _customerDetails)
     {
+        String textCustomerDetails = pageDetailFatura.getTextCustomerDetails();
+        Assert.assertEquals(_customerDetails, textCustomerDetails);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Room com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_room_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_room_com(String _room)
     {
+        String textRoom = pageDetailFatura.getTextRoom();
+        Assert.assertEquals(_room, textRoom);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Check In com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_check_in_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_check_in_com(String _checkIn)
     {
+        String textCheckIn = pageDetailFatura.getCheckIn();
+        Assert.assertEquals(_checkIn, textCheckIn);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Check Out com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_check_out_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_check_out_com(String _checkOut)
     {
+        String textCheckOut = pageDetailFatura.getCheckOut();
+        Assert.assertEquals(_checkOut, textCheckOut);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Total Stay Count com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_total_stay_count_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_total_stay_count_com(String _stayCount)
     {
+        String textStayCount = pageDetailFatura.getStayCount();
+        Assert.assertEquals(_stayCount, textStayCount);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Total Stay Amount com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_total_stay_amount_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_total_stay_amount_com(String _stayAmount)
     {
+        String textStayAmount = pageDetailFatura.getStayAmount();
+        Assert.assertEquals(_stayAmount, textStayAmount);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Deposit Now com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_deposit_now_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_deposit_now_com(String _billingDetails)
     {
+        String textBillingDetails = pageDetailFatura.getBillingDetails();
+        Assert.assertEquals(_billingDetails, textBillingDetails);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Tax & VATe com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_tax_va_te_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_tax_va_te_com(String _taxVat)
     {
+        String textTaxVat = pageDetailFatura.getTaxVat();
+        Assert.assertEquals(_taxVat, textTaxVat);
     }
 
     @Entao("a pagina Invoice Detail exibe o campo Total Amount com {string}")
-    public void a_pagina_invoice_details_exibe_o_campo_total_amount_com(String string)
+    public void a_pagina_invoice_details_exibe_o_campo_total_amount_com(String _totalAmount)
     {
+        String textTotalAmount = pageDetailFatura.getTotalAmount();
+        Assert.assertEquals(_totalAmount, textTotalAmount);
     }
 
 }
