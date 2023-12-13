a7c05c3c6318eaf9ebdb76a00ea2f4435af8118b
Edinete Sousa <edineteb.sousa@gmail.com>
2023-12-12 22:03:04 -0300

    Subindo Pages

--- src/test/java/pages/Grupo1_Heroherokuapp_Login.java
+++ src/test/java/pages/Grupo1_Heroherokuapp_Login.java
@@ -1,5 +1,11 @@
 package pages;
 
-public class Grupo1_Heroherokuapp_Login {
+import org.openqa.selenium.By;
 
+public class Grupo1_Heroherokuapp_Login {
+    private By campo_username = By.id("username");
+    private By campo_password = By.id("password");
+    private By btn_login = By.className("radius");
+    private By valida_msg = By.id("flash");
+    private By btn_logout = By.linkText("/logout");
 }
