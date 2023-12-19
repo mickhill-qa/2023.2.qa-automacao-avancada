7ee197114f096bb320389a06b4b322c4b1aadeeb
Edmozer Cavalcante <edmozercavalcante@Edmozers-MacBook-Pro.local>
2023-12-19 16:26:52 -0700

    att

--- src/test/java/pages/Equipe3HackathonSeuBarrigaLogin.java
+++ src/test/java/pages/Equipe3HackathonSeuBarrigaLogin.java
@@ -3,7 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 import runner.base_class.BasePage;
 
-public class HackathonSeuBarrigaLogin extends BasePage {
+public class Equipe3HackathonSeuBarrigaLogin extends BasePage {
 
     private String url = "https://seubarriga.wcaquino.me/logout";
 
---
+++ src/test/java/steps/equipe3SeuBarrigaLoginSteps.java
@@ -0,0 +1,21 @@
+package steps;
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+import runner.base_class.BaseSteps;
+
+public class equipe3SeuBarrigaLoginSteps extends BaseSteps {
+
+    @Given("the SeuBarriga login page is loaded")
+    public void the_seu_barriga_login_page_is_loaded() {
+
+
+    }
+    @When("the user inserts valid credentials and clicks on the login button")
+    public void the_user_inserts_valid_credentials_and_clicks_on_the_login_button(String _username, String _password) {
+
+    }
+    @Then("the system will redirect you to the home page")
+    public void the_system_will_redirect_you_to_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+}
