314cdf6659a6fdbafce6c55654b3ea16e6b97ec7
arthurmgui <arthurmottaguimaraes@outlook.com>
2023-12-19 20:37:31 -0300

    arquivo steps e pagina de home criadas

---
+++ src/test/java/steps/Grupo4LoginSrBarrigaSteps.java
@@ -0,0 +1,40 @@
+package steps;
+
+import io.cucumber.java.pt.*;
+import runner.base_class.BaseSteps;
+
+public class Grupo4LoginSrBarrigaSteps extends BaseSteps {
+
+    //Cenario 1 e 2
+    @Dado("que o usuario esta na pagina de login")
+    public void que_o_usuario_esta_na_pagina_de_login() {
+
+    }
+
+    @Quando("o usuario preenche o email {string}")
+    public void o_usuario_preenche_o_email(String string) {
+
+    }
+
+    @Quando("o usuario preeche a senha {string}")
+    public void o_usuario_preeche_a_senha(String string) {
+
+    }
+
+    @Quando("aperta o botao de entrar")
+    public void aperta_o_botao_de_entrar() {
+
+    }
+
+    @Entao("ele redireciona para pagina incial")
+    public void ele_redireciona_para_pagina_incial() {
+
+    }
+
+    //Cenario 3,4,5 e 6
+    @Entao("ele exibe a mensagem de erro {string}")
+    public void ele_exibe_a_mensagem_de_erro(String string) {
+
+    }
+
+}
